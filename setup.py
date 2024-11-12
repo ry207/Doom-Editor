3b707a9e9af841eae683b659c5ee66c12534a4bf
ryperson <80601374+ry207@users.noreply.github.com>
2024-11-12 22:26:26 +0000

    Delete setup.py

--- setup.py
+++
@@ -1,6 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(name = "Doom Editor" ,
-      version = "1.0" ,
-      description = "Doom Text Editor" ,
-      executables = [Executable("main.py")])
